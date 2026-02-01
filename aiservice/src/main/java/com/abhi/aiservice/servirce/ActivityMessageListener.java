4593c2fd26a3b2217f7b490b4916754273eaf8a4
abhishekmali09 <abhishekmali7710@gmail.com>
2026-02-01 18:03:13 +0530

    AI service is created

--- aiservice/src/main/java/com/abhi/aiservice/servirce/ActivityMessageListener.java
+++ aiservice/src/main/java/com/abhi/aiservice/servirce/ActivityMessageListener.java
@@ -11,10 +11,12 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ActivityMessageListener {
 
+    private final ActivityAIService activityAIService;
+
     @RabbitListener(queues ="activity.queue")
     public void processActivity(Activity activity){
         log.info("Received activity for processing: {}", activity.getId());
-
+        log.info("Genrated Recommendation: {}", activityAIService.generateRecommedation(activity));
 
     }
 
